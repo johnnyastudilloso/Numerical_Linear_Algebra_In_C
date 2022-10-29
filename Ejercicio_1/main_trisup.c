cb2015815f1cab4554b42b1932b0bc50a0e26248
johnnyastudilloso <johnnyastudilloso@gmail.com>
2022-10-30 01:11:53 +0200

    Traspaso de resoltrisup a linalg.c

--- Ejercicio_1/main_trisup.c
+++ Ejercicio_1/main_trisup.c
@@ -5,45 +5,6 @@
 #include "../dependences/prod_escalar.c"
 #include "../dependences/linalg.c"
 
-/**
- * Resolvemos una matriz a mediante substitución hacia atrás
- */
-int resoltrisup(int m, int n, double **a, double *x, double tol)
-{
-    /* Variables de recorrido */
-    int i, j;
-    double sum;
-
-    /* Comprobamos que las dimensiones de la matriz sean correctas */
-    if(n != m + 1)
-    {
-        printf("\nNumero de filas y columnas incorrecto:\nn = m + 1");
-        return 1;
-    }
-    for(i = 0; i < m; i++)
-    {
-        if(fabs(a[i][i]) < tol)
-        {
-            printf("\na[%d][%d]: %f < tolerancia: %f", i, i, a[i][i], tol);
-            return 1;
-        }
-    }
-    /* Guardamos el resultado de la primera ecución (resolución directa) */
-    x[m - 1] = a[m - 1][n - 1]/a[m - 1][m - 1];
-
-    /* Resolvemos el resto de equaciones en funcion de la inicial de forma iterativa */
-    for(i = m - 2; i >= 0; i--)
-    {
-        sum = 0;
-        for (j = m - 1; j > i; j--)
-        {
-            sum += a[i][j] * x[j];
-        }
-        x[i] = (a[i][n - 1] - (sum)) / a[i][i];
-    }
-    return 0;
-}
-
 int main(void)
 {
     int i, j, m, n, k;
--- dependences/linalg.c
+++ dependences/linalg.c
@@ -1,3 +1,42 @@
+/**
+ * Resolvemos una matriz a mediante substitución hacia atrás
+ */
+int resoltrisup(int m, int n, double **a, double *x, double tol)
+{
+    /* Variables de recorrido */
+    int i, j;
+    double sum;
+
+    /* Comprobamos que las dimensiones de la matriz sean correctas */
+    if(n != m + 1)
+    {
+        printf("\nNumero de filas y columnas incorrecto:\nn = m + 1");
+        return 1;
+    }
+    for(i = 0; i < m; i++)
+    {
+        if(fabs(a[i][i]) < tol)
+        {
+            printf("\na[%d][%d]: %f < tolerancia: %f", i, i, a[i][i], tol);
+            return 1;
+        }
+    }
+    /* Guardamos el resultado de la primera ecución (resolución directa) */
+    x[m - 1] = a[m - 1][n - 1]/a[m - 1][m - 1];
+
+    /* Resolvemos el resto de equaciones en funcion de la inicial de forma iterativa */
+    for(i = m - 2; i >= 0; i--)
+    {
+        sum = 0;
+        for (j = m - 1; j > i; j--)
+        {
+            sum += a[i][j] * x[j];
+        }
+        x[i] = (a[i][n - 1] - (sum)) / a[i][i];
+    }
+    return 0;
+}
+
 /* Esta funcion calcula el vector residuo de un sistema lineal */
 void residu(int m, int n, double **a, double *x, double *r)
 {
