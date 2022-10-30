4e18499f55054077ac83d1786b273660667dca04
johnnyastudilloso <johnnyastudilloso@gmail.com>
2022-10-30 15:36:47 +0100

    Corrigiendo estructura proyecto y creando ficheros de ultimos ejercicios

--- linalg.c
+++ linalg.c
@@ -1,4 +1,33 @@
-/**
+float prod_esc(int n, double *a, double*b)
+{
+    int i;
+    float prod = 0.f;
+
+    for(i = 0; i < n; i++)
+    {
+        prod += a[i] * b[i];
+    }
+
+    return prod;
+}
+
+void prodMatVec(int m, int n, double **a, double *x, double *r)
+{
+    int i, j;
+    double sum;
+
+    for(i = 0; i < m; i++)
+    {
+        sum = 0;
+        for(j = 0; j < n; j++)
+        {
+            sum += a[i][j] * x[j];
+        }
+        r[i] = sum;
+    }
+}
+
+/*
  * Resolvemos una matriz a mediante substitución hacia atrás
  */
 int resoltrisup(int m, int n, double **a, double *x, double tol)
